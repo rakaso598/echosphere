8b6da2ce74498129ebce6862a14c13952ce0f7de
rakaso598 <nexonaddress98@gmail.com>
2025-09-04 09:37:50 +0900

    fix: update dotenv config path for improved portability

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import dotenv from 'dotenv';
-dotenv.config({ path: path.resolve(process.cwd(), 'apps/api/.env') });
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 import express, { Request, Response } from 'express';
 import cors from 'cors';
